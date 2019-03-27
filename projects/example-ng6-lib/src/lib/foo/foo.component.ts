abbf90c89c5ff3dd426c9990a83886b969bfedc8
mskluev <mskluev@skogul.com>
2019-03-27 15:55:47 -0400

    basic d3 example working

--- projects/example-ng6-lib/src/lib/foo/foo.component.ts
+++ projects/example-ng6-lib/src/lib/foo/foo.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import * as cloud from 'd3-cloud';
+import * as d3 from 'd3';
 
 @Component({
-  selector: 'enl-foo',
-  templateUrl: './foo.component.html',
-  styleUrls: ['./foo.component.css']
+    selector: 'enl-foo',
+    templateUrl: './foo.component.html',
+    styleUrls: ['./foo.component.css']
 })
 export class FooComponent implements OnInit {
+    private words = ['Hello', 'world', 'normally', 'you', 'want', 'more', 'words', 'than', 'this'].map(function(d) {
+        return { text: d, size: 10 + Math.random() * 90 };
+    });
 
-  constructor() { }
+    constructor() {}
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        console.log('Hello Debugger');
+        /*d3.layout
+            .cloud()
+            .size([960, 500])
+            .words(this.words)
+            .padding(5)
+            .rotate(~~(Math.random() * 2) * 90)
+            .font('Impact')
+            .fontSize(d => d.size())
+            .on('end', this.end)
+            .start();
+            */
+    }
 
+    end(words) {
+        console.log(JSON.stringify(words));
+    }
+
+    clicked(event: any) {
+        d3.select(event.target)
+            .append('circle')
+            .attr('cx', event.offsetX)
+            .attr('cy', event.offsetY)
+            .attr('r', () => {
+                return 10;
+            })
+            .attr('fill', 'red');
+    }
 }
--- projects/example-ng6-tester/src/app/app.component.ts
+++ projects/example-ng6-tester/src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'example-ng6-tester';
+export class AppComponent implements OnInit {
+    title = 'example-ng6-tester';
+
+    ngOnInit() {
+        console.log('Hello Debugger 2');
+    }
 }
