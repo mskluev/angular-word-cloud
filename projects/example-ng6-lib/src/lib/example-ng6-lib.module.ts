3999177813c89c779cd827ab1191c8ffc8df8fd8
mskluev <mskluev@skogul.com>
2019-04-16 13:24:07 -0400

    started force-graph

--- projects/example-ng6-lib/src/lib/example-ng6-lib.module.ts
+++ projects/example-ng6-lib/src/lib/example-ng6-lib.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { ExampleNg6LibComponent } from './example-ng6-lib.component';
 import { FooComponent } from './foo/foo.component';
 import { WorldCloudComponent } from './world-cloud/world-cloud.component';
+import { ForceGraphComponent } from './force-graph/force-graph.component';
 
 @NgModule({
-    declarations: [ExampleNg6LibComponent, FooComponent, WorldCloudComponent],
+    declarations: [ExampleNg6LibComponent, FooComponent, WorldCloudComponent, ForceGraphComponent],
     imports: [],
-    exports: [ExampleNg6LibComponent, FooComponent, WorldCloudComponent]
+    exports: [ExampleNg6LibComponent, FooComponent, WorldCloudComponent, ForceGraphComponent]
 })
 export class ExampleNg6LibModule {}
---
+++ projects/example-ng6-lib/src/lib/force-graph/force-graph.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'enl-force-graph',
+  templateUrl: './force-graph.component.html',
+  styleUrls: ['./force-graph.component.css']
+})
+export class ForceGraphComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
